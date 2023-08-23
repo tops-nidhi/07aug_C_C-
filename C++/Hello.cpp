024c19cd258969696dccbb1c77ae93fdca064669
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-08-23 13:04:38 +0530

    add

---
+++ C++/Hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main()
+{
+    int n;
+    std::cout<<"Enter the value of the n: "; // To take the input from the user
+    std::cin>>n;
+    std::cout<<std::endl<<"\nValue if the n is: "<<n;
+    return 0;
+}
