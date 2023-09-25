e61f2db07dbaac09fdbc5541002b88c912b2ca8f
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-09-25 12:25:21 +0530

    add

---
+++ C++/Polymorphisam/Operator-Overloading.cpp
@@ -0,0 +1,39 @@
+// WAP to operator overloading
+#include <iostream>  // cout, endl
+using namespace std;
+class Operator
+{
+public: 
+    int no;
+    void get()
+    {
+        cout<<"Enter the value of no: ";
+        cin>>no;
+    }
+    // Operator operator ++()
+    // {
+    //     no++;
+    // }
+    Operator operator +(Operator &obj2)
+    {
+        Operator obj3;
+        obj3.no = no + obj2.no;
+        return (obj3);
+    }
+    void display()
+    {
+        cout<<"Value of the no is: "<<no<<endl;
+    }
+};
+int main()
+{
+    Operator operate1, operate2, operate3;
+    operate1.get();
+    operate1.display();
+    operate2.get();
+    operate2.display();
+    // ++operate;
+    operate3 = operate1 + operate2;
+    operate3.display();
+    return 0;
+}
